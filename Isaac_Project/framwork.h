44d29c9532c4c5f450902544eef8a4a788589778
华佳浩 <2844344532@qq.com>
2025-04-30 11:16:03 +0800

    准备重构

---
+++ Isaac_Project/framwork.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+// C 头文件
+
+
+// C++ 头文件
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+// 第三方库
+
+
+//SDL头文件
+#include <../include/SDL2/SDL.h>
+#include <../include/SDL2/SDL_image.h>
+#include <../include/SDL2/SDL_mixer.h>
+#include <../include/SDL2/SDL_ttf.h>
+
+//ffmpeg头文件
+#ifdef __cplusplus
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#endif
+#ifdef __cplusplus
+}
+#endif
